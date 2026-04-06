1505607136206a988dce30b5a6792c6b3c7d7856
Metecdr <mete.aksungur07@gmail.com>
2026-04-06 02:50:15 +0000

    PostgreSQL migration, SSL kurulumu, admin panel fix

---
+++ quiz/migrations/0016_karsilasmaodasi_oda_kodu_and_more.py
@@ -0,0 +1,10 @@
+from django.db import migrations, models
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('quiz', '0015_karsilasmaodasi_bitis_zamani'),
+    ]
+
+    operations = [
+    ]
